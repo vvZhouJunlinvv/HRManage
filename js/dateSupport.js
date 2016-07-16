9333ab295ef59290f8f6107b537f792ea0f248df
zhoujunlin <539513388@qq.com>
2016-07-16 10:32:08 +0800

    Init

---
+++ js/dateSupport.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+
+    $('#data_1 .input-group.date').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true
+    });
+
+    $('#data_2 .input-group.date').datepicker({
+        startView: 1,
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        autoclose: true
+    });
+
+    $('#data_3 .input-group.date').datepicker({
+        startView: 2,
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        autoclose: true
+    });
+
+    $('#data_4 .input-group.date').datepicker({
+        minViewMode: 1,
+        keyboardNavigation: false,
+        forceParse: false,
+        autoclose: true,
+        todayHighlight: true
+    });
+
+    $('#data_5 .input-daterange').datepicker({
+        keyboardNavigation: false,
+        forceParse: false,
+        autoclose: true
+    });
+
+    var elem = document.querySelector('.js-switch');
+    var switchery = new Switchery(elem, {
+        color: '#1AB394'
+    });
+
+    var elem_2 = document.querySelector('.js-switch_2');
+    var switchery_2 = new Switchery(elem_2, {
+        color: '#ED5565'
+    });
+
+    var elem_3 = document.querySelector('.js-switch_3');
+    var switchery_3 = new Switchery(elem_3, {
+        color: '#1AB394'
+    });
+
+});
+var config = {
+    '.chosen-select': {},
+    '.chosen-select-deselect': {
+        allow_single_deselect: true
+    },
+    '.chosen-select-no-single': {
+        disable_search_threshold: 10
+    },
+    '.chosen-select-no-results': {
+        no_results_text: 'Oops, nothing found!'
+    },
+    '.chosen-select-width': {
+        width: "95%"
+    }
+}
+
